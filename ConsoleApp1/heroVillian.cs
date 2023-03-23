c48aa58f06f773b15af8388586fe4887bddd4538
Immanuel <nowpertimmanuel@gmail.com>
2023-03-23 03:31:00 -0700

    heroVillian problem solution updated

---
+++ ConsoleApp1/heroVillian.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main(string[] args)
+	{
+		int totalNoOfBullets = 100;
+		int heroFires = 1;
+		while(totalNoOfBullets > 0)
+		{
+			Console.WriteLine("hero fires " + heroFires);
+			Console.WriteLine($"villian fires {heroFires+1} {heroFires + 2} {heroFires + 3}");
+			heroFires+=4;
+			totalNoOfBullets -= 4;
+			
+		}
+	}
+}
