96f25898a264faaf6d70bfc48ee0ec55f036510f
Immanuel <nowpertimmanuel@gmail.com>
2023-03-27 23:28:13 -0700

    lambda tried

--- ConsoleApp1/DelegatesTry.cs
+++ ConsoleApp1/DelegatesTry.cs
@@ -11,30 +11,30 @@ namespace ConsoleApp1
         public delegate int DelegateForSum(int a ,int b);
         public delegate T DelegateForGeneric<T> (T a,T  b);
         public delegate T DelegateNoParams<T>();
-        public static void Main(string[] args)
-        {
-            DelegateForSum delegateForSum = Sum;
-            delegateForSum += Diff;
+        //public static void Main(string[] args)
+        //{
+        //    DelegateForSum delegateForSum = Sum;
+        //    delegateForSum += Diff;
 
-            //generic delegates
-            DelegateForGeneric<int> dt = GenericTryFun1;
-            DelegateForGeneric<string> dt1 = GenericTryFun2;
+        //    //generic delegates
+        //    DelegateForGeneric<int> dt = GenericTryFun1;
+        //    DelegateForGeneric<string> dt1 = GenericTryFun2;
 
-            //anonymus functions
-            DelegateForGeneric<int> anonymusDelegate = new(delegate (int a, int b) { return a + b; });
+        //    //anonymus functions
+        //    DelegateForGeneric<int> anonymusDelegate = new(delegate (int a, int b) { return a + b; });
 
-            DelegateForGeneric<string>anonymusDelegate1=new(delegate (string a, string b) {  return a + b; });
+        //    DelegateForGeneric<string>anonymusDelegate1=new(delegate (string a, string b) {  return a + b; });
 
-            DelegateNoParams<string> anonymusDelegate2 = new(delegate () { return "none"; });
+        //    DelegateNoParams<string> anonymusDelegate2 = new(delegate () { return "none"; });
 
 
-            foreach (DelegateForSum d in delegateForSum.GetInvocationList()) {
-                Console.WriteLine(d.Invoke(3,4));
-            }
+        //    foreach (DelegateForSum d in delegateForSum.GetInvocationList()) {
+        //        Console.WriteLine(d.Invoke(3,4));
+        //    }
 
 
             
-        }
+        //}
 
 
 
--- ConsoleApp1/LambdaExpressionsTry.cs
+++ ConsoleApp1/LambdaExpressionsTry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,47 @@ namespace ConsoleApp1
 {
     internal class LambdaExpressionsTry
     {
+        delegate T GenericDelegate<T>(T a ,T b);
+        public static void Main(string[] args)
+        {
+            GenericDelegate<int> lambaAdd = (num1, num2) => num1 + num2;
+            Console.WriteLine(lambaAdd(2, 3));
+
+            //lambda multicast
+            var lambdaWODelegate = (int num1,int num2) => num1 + num2;
+
+            lambdaWODelegate += (int num1, int num2) => num1 - num2;
+            Console.WriteLine(lambdaWODelegate(2, 3));
+
+
+
+
+            foreach (var i in lambdaWODelegate.GetInvocationList())
+            {
+                Console.WriteLine(i.Method);
+                Console.WriteLine(i.DynamicInvoke(3, 4));
+            }
+
+            //lambda use cases
+
+            List<int> someList = new() { 19, 5, 3, 2, 35, 10, 34, 60 };
+            var square = (int num1) => num1 * num1;
+
+            foreach (int i in someList)
+               Console.WriteLine(square(i));
+
+            Console.WriteLine(someList.Aggregate((int num1, int num2) => num1 + num2));
+
+            Console.WriteLine(someList.All((int a) => a > 10));
+
+            //add all elements divisble by 5 in new list
+            List<int>divisbleBy5=someList.FindAll(a => a %5==0);
+
+            foreach (int i in divisbleBy5)
+                Console.Write(i + " ");
+   
+        }
+
+       
     }
 }
